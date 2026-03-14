a06508f451f3654b786d7d7857d66475bcf86ea6
praveen0309d <d.praveen0309@gmail.com>
2026-03-14 12:05:22 +0530

    nnn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home.js";
 import AboutUs from "./components/About.js";
 import Chatbot from "./components/Chatbot.js";
-// import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
-    {/* <Analytics/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
+
       <Chatbot />
+      <Analytics />
     </>
   );
 }
